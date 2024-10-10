4646ed2b6a46670fd3a591b7d3aad8481419f6f4
Damiao Netto <143633691+DamiaoNetto@users.noreply.github.com>
2024-10-10 01:10:21 -0300

    ajustando detalhes finais

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,38 @@ var typed = new Typed(".text", {
     loop: true
 })
 
+let btnMenu = document.getElementById("btn-menu")
+let menu = document.getElementById("menu-mobile")
+let overlay = document.getElementById("overlay-menu")
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add("abrir-menu")
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove("abrir-menu")
+})
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove("abrir-menu")
+})
+
+let btnMostrarProjetos = document.querySelector('.btn-mostrar-projetos')
+let projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
+
+btnMostrarProjetos.addEventListener('click',()=>{
+    mostrarProjetos()
+
+})
+
+function mostrarProjetos(){
+    projetosInativos.forEach(projetoInativo =>{
+        projetoInativo.classList.toggle('ativo')
+
+    })
+    btnMostrarProjetos.textContent = btnMostrarProjetos.textContent === "MOSTRAR MAIS" ? "MOSTRAR MENOS" : "MOSTRAR MAIS";
+
+    
+}
+
+
+
